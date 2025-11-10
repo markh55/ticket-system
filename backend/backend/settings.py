47b0e754681286f3d84ac5c7598e8b7efee642c0
Mark Hallett <markhallett5@outlook.com>
2025-11-10 21:44:58 +0000

    Added custom User model and registered in admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'emails',
 ]
 
+AUTH_USER_MODEL = 'users.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
