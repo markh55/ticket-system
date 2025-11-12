bf39d1c0d5f4181f6b5d2b53ff33f55f4a1855cd
Mark Hallett <markhallett5@outlook.com>
2025-11-12 21:10:59 +0000

    added backend/urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from emails.views import mailgun_webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/tickets/', include('tickets.urls')),
     path('api/emails/', include('emails.urls')),
+    path('webhook/', mailgun_webhook, name='mailgun-webhook'),
 ]
